aeacd039ea7999db656d56b58f150987c5571f25
mantu_nguyen <nguyen.mantuphan@gmail.com>
2017-10-24 16:06:15 -0500

    add splash page

--- react-bootstrap/docs/src/HomePage.js
+++ react-bootstrap/docs/src/HomePage.js
@@ -6,6 +6,7 @@ import Grid from '../../src/Grid';
 import Alert from '../../src/Alert';
 import Glyphicon from '../../src/Glyphicon';
 import Label from '../../src/Label';
+import Carousel from '../../src/Carousel';
 
 export default class HomePage extends React.Component {
   render() {
@@ -13,28 +14,20 @@ export default class HomePage extends React.Component {
       <div>
         <NavMain activePage="home" />
 
-        <main className="bs-docs-masthead" id="content" role="main">
-          <div className="container">
-            <span className="bs-docs-booticon bs-docs-booticon-lg bs-docs-booticon-outline" />
-            <p className="lead">The most popular front-end framework, rebuilt for React.</p>
-          </div>
-        </main>
-
-        <Grid>
-          <Alert bsStyle="warning">
-            <p><Glyphicon glyph="bullhorn" /> We are actively working to reach a 1.0.0 release, and we would love your help to get there.</p>
-            <p><Glyphicon glyph="check" /> Check out the <a href="https://github.com/react-bootstrap/react-bootstrap/wiki#100-roadmap">1.0.0 roadmap</a> and <a href="https://github.com/react-bootstrap/react-bootstrap/blob/master/CONTRIBUTING.md">contributing guidelines</a> to see where you can help out.</p>
-            <p><Glyphicon glyph="sunglasses" /> A great place to start is any <a target="_blank" href="https://github.com/react-bootstrap/react-bootstrap/issues?q=is%3Aopen+is%3Aissue+label%3A%22help+wanted%22">issue</a> with a <Label bsStyle="success">help-wanted</Label> label.</p>
-            <p><Glyphicon glyph="ok" /> We are open to pull requests that address bugs, improve documentation, enhance accessibility, add test coverage, or bring us closer to feature parity with <a target="_blank" href="http://getbootstrap.com/">Bootstrap</a>.</p>
-            <p><Glyphicon glyph="user" /> We actively seek to invite frequent pull request authors to join the organization. <Glyphicon glyph="thumbs-up" /></p>
-          </Alert>
-          <Alert bsStyle="danger">
-            <p><Glyphicon glyph="warning-sign" /> The project is under active development, and APIs will change. </p>
-            <p><Glyphicon glyph="bullhorn" /> Prior to the 1.0.0 release, breaking changes should result in a minor version bump.</p>
-          </Alert>
-        </Grid>
-
-        <PageFooter />
+        <div>
+          <Carousel>
+            <Carousel.Item>
+              <img className={"carousel"} src="http://animals.sandiegozoo.org/sites/default/files/2016-10/animals_hero_penguin_02_1.jpg" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="http://www.cambridgeblog.org/wp-content/uploads/2015/05/What-is-an-Animal.jpg" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img src="http://animals.sandiegozoo.org/sites/default/files/2016-10/animals_hero_penguin_02_1.jpg" />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        
       </div>
     );
   }
