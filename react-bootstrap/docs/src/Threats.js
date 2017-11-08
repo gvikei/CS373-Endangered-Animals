fedd04a2a3d6f374d18335cd60f3b89cef7a0a50
shivavelingker <svelingker@utexas.edu>
2017-11-08 00:07:40 -0600

    Convert Threats Model to grid with IDB3 specs

--- react-bootstrap/docs/src/Threats.js
+++ react-bootstrap/docs/src/Threats.js
@@ -3,14 +3,7 @@ import React from 'react';
 import NavMain from './NavMain';
 import PageHeader from './PageHeader';
 import PageFooter from './PageFooter';
-import Row from '../../src/Row';
-import Col from '../../src/Col';
-import Thumbnail from '../../src/Thumbnail';
-import Image from '../../src/Image';
-import Panel from '../../src/Panel';
-import Button from '../../src/Button';
-import Collapse from '../../src/Collapse';
-import Well from '../../src/Well';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 var axios = require('axios');
 
@@ -19,78 +12,113 @@ class Threats extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: true,
-      threats: []
+        open: true,
+        threats: [],
+        pages: null,
+        loading: true
     };
+    this.instanceFormatter = this.instanceFormatter.bind(this);
+    this.animalFormatter = this.animalFormatter.bind(this);
+    this.habitatFormatter = this.habitatFormatter.bind(this);
 
     var that = this;
-      axios.create({
-        baseURL: 'https://swe-endangered-animals.appspot.com/',
-        headers: {"Access-Control-Allow-Origin": "*"}
-      }).get('/all_threat_data')
-        .then(function(data) {
-          console.log(data.data);
-          that.setState({
-            threats: data.data.slice(0,10)
-          });
-      });
-  }
-
-
-  changeURL(type, data) {
-      if(typeof data !== "undefined")
-        global.instance = data;
-      this.context.router.push('/'+type+".html/");
+    axios.create({
+      baseURL: 'https://swe-endangered-animals.appspot.com/',
+      headers: {"Access-Control-Allow-Origin": "*"}
+    }).get('/all_threat_data')
+      .then(function(data) {
+        that.setState({
+          threats: data.data
+        });
+    });
   };
 
   shouldComponentUpdate() {
     return true;
-  }
+  };
 
-  renderThreats(threat) {
+
+  changeURL(type, data){
+    if(typeof data !== "undefined")
+      global.instance = data;
+    this.context.router.push('/'+type+".html/");
+  };
+
+  animalFormatter(list){
+    var type = "animal";
+
+    var links = list.map(function(x, i){
+      return ( <li key={type+x+i}> { this.instanceFormatter(x, null, type) } </li> );
+    }.bind(this));
+
     return (
-
-      <Col sm={3} className="bs-docs-body">
-        <h4><a onClick={ () => { this.changeURL("threat", threat.name) } } >{ threat.name }</a></h4>
-        <p><img src={threat.image }/></p>
-        <p>Score: {threat.score }</p>
-        <p>Timing: { threat.timing }</p>
-      </Col>
+      <div>
+        <ul>
+          { links }
+        </ul>
+      </div>
     );
   };
 
+  habitatFormatter(list){
+    var type = "habitat";
+    
+    var links = list.map(function(x, i){
+      return ( <li key={type+x+i}> { this.instanceFormatter(x, null, type) } </li> );
+    }.bind(this));
+
+    return (
+      <div>
+        <ul>
+          { links }
+        </ul>
+      </div>
+    );
+  };
+
+  imageFormatter(data){
+    return <img src={ data } height="250px" width="250px" />;
+  };
+
+  instanceFormatter(data, row, type){
+    if(typeof type == "undefined")
+      type = "threat";
+    return <a onClick={ () => { this.changeURL(type, data) } } >{ data }</a>;
+  };
+
+  linkFormatter(data){
+    return <a href={ data } target="_blank">{ data }</a>
+  };
+
   render() {
+    if(!this.state.threats.length)
+      return ( <div /> )
+
     return (
       <div>
         <NavMain activePage="threats" />
 
-        <PageHeader
+         <PageHeader
           title="Threats"
           subTitle=""/>
 
-          <div className="container-fluid">
-
-          <Row className="container-fluid">
-            {
-
-                this.state.threats.map(function(threat, i){
-                return this.renderThreats(threat);
-              }.bind(this))
-
-            }
-          </Row>
-
-
-        </div>
+           <BootstrapTable data={this.state.threats} striped={true} hover={true} ref='table' pagination={true} search={true} columnFilter={true}>
+            <TableHeaderColumn dataField="image"            dataAlign="center"                                dataFormat={this.imageFormatter}    > Image                 </TableHeaderColumn>
+            <TableHeaderColumn dataField="name"             dataAlign="center" dataSort={true} isKey={true}   dataFormat={this.instanceFormatter} > Name                  </TableHeaderColumn>
+            <TableHeaderColumn dataField="severity"         dataAlign="center" dataSort={true}                                                    > Severity              </TableHeaderColumn>
+            <TableHeaderColumn dataField="timing"           dataAlign="center" dataSort={true}                                                    > Timing                </TableHeaderColumn>
+            <TableHeaderColumn dataField="assoc_animals"    dataAlign="center"                                dataFormat={this.animalFormatter}   > Associated Animals    </TableHeaderColumn>
+            <TableHeaderColumn dataField="assoc_habitats"   dataAlign="center"                                dataFormat={this.habitatFormatter}  > Associated Habitats   </TableHeaderColumn>
+          </BootstrapTable>
 
         <PageFooter />
       </div>
-    );
+  );
   }
 }
 
 Threats.contextTypes = {
-      router: React.PropTypes.object.isRequired
-  };
+    router: React.PropTypes.object.isRequired
+};
 
 export default Threats;
