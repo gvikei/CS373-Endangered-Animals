065393c5a04b8a42701a972ad1e8e551ce2ff27e
Khuyen Duong <truckhuyen@gmail.com>
2017-11-26 23:24:33 -0600

    Added some more tests

--- front-end/unit_tests/tests.py
+++ front-end/unit_tests/tests.py
@@ -14,11 +14,26 @@ class UnitTests(unittest.TestCase):
         self.browser.get('http://endangered-animals.me/about.html')
         self.assertIn('About Us', self.browser.page_source)
 
+    def test_animals_page_is_accessible(self):
+        self.browser.get('http://endangered-animals.me/animals.html')
+        self.assertIn('Animals', self.browser.page_source)
+
+    def test_threats_page_is_accessible(self):
+        self.browser.get('http://endangered-animals.me/threats.html')
+        self.assertIn('Threats', self.browser.page_source)
+
+    # def test_countries_page_is_accessible(self):
+    #     self.browser.get('http://endangered-animals.me/countries.html')
+    #     self.assertIn('Countries', self.browser.page_source)
+    #
+    # def test_habitats_page_is_accessible(self):
+    #     self.browser.get('http://endangered-animals.me/habitats.html')
+    #     header_element = self.browser.find_element_by_class_name("bs-docs-header")
+    #     self.assertIn('Habitats', header_element.get_attribute('innerHTML'))
+
     def test_homepage_is_accessible(self):
         self.browser.get('http://endangered-animals.me')
         self.assertIn('Planet Animal', self.browser.title)
-
-
 
 if __name__ == '__main__':
     unittest.main()
