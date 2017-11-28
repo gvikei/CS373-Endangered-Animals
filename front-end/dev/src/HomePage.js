94e4a6d67ecea004c7fcc13a11b2a4a0152c7bbd
Khuyen Duong <truckhuyen@gmail.com>
2017-11-27 22:20:36 -0600

    Added modal to show search result

--- front-end/dev/src/HomePage.js
+++ front-end/dev/src/HomePage.js
@@ -7,12 +7,71 @@ import Alert from '../../components/Alert';
 import Glyphicon from '../../components/Glyphicon';
 import Label from '../../components/Label';
 import Carousel from '../../components/Carousel';
+import Modal from '../../components/Modal';
+import FormControl from '../../components/FormGroup';
+import FormGroup from '../../components/FormControl';
+import Button from '../../components/Button';
 
-export default class HomePage extends React.Component {
+
+const MyLargeModal = React.createClass({
   render() {
     return (
+      <Modal {...this.props} bsSize="large" aria-labelledby="contained-modal-title-lg">
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-lg">Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Wrapped Text</h4>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  },
+});
+
+
+
+export default class HomePage extends React.Component {
+
+  handleClick(e) {
+    this.setState({
+      lgModalShow : !this.state.lgModalShow
+    });
+    console.log('lgModalShow', this.state.lgModalShow);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lgModalShow: false
+    }
+  }
+
+  render() {
+    let lgClose = () => this.setState({ lgModalShow: false });
+
+    return (
       <div>
-        <NavMain activePage="home" />
+        <NavMain activePage="home"/>
+
+          <FormGroup>
+          </FormGroup>
+          {' '}
+          <Button type="submit" onClick={ ()=> {this.handleClick()}} >
+            Search
+          </Button>
+          <MyLargeModal show={this.state.lgModalShow} onHide={lgClose} />
 
         <div>
           <Carousel>
