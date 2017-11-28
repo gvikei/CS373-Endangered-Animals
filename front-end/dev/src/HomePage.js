ca490e479b2f188107ae3a0575c22a9ca6e02bcb
Khuyen Duong <truckhuyen@gmail.com>
2017-11-27 22:40:23 -0600

    Added one model to the search page

--- front-end/dev/src/HomePage.js
+++ front-end/dev/src/HomePage.js
@@ -11,10 +11,33 @@ import Modal from '../../components/Modal';
 import FormControl from '../../components/FormGroup';
 import FormGroup from '../../components/FormControl';
 import Button from '../../components/Button';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
+var axios = require('axios');
 
 const MyLargeModal = React.createClass({
   render() {
+    const options = {
+      page: 1,
+      sizePerPageList: [ {
+        text: '10', value: 10
+      }, {
+        text: '20', value: 20
+      }, {
+        text: '30', value: 30
+      } ],
+      sizePerPage: 10,
+      pageStartIndex: 1,
+      paginationSize: 5,
+      prePage: 'Prev',
+      nextPage: 'Next',
+      firstPage: 'First',
+      lastPage: 'Last',
+      paginationShowsTotal: this.renderShowsTotal,
+      paginationPosition: 'top'
+    };
+
+
     return (
       <Modal {...this.props} bsSize="large" aria-labelledby="contained-modal-title-lg">
         <Modal.Header closeButton>
@@ -22,15 +45,21 @@ const MyLargeModal = React.createClass({
         </Modal.Header>
         <Modal.Body>
           <h4>Wrapped Text</h4>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
-          <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-          <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+           <BootstrapTable data={this.props.model} striped={true} hover={true} ref='table' pagination={true} search={true} columnFilter={true} options={options}>
+
+            <TableHeaderColumn width='200' dataField="imageLink"            dataAlign="center"                   dataFormat={this.imageFormatter}                 > Image                </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="name"                 dataAlign="center"  dataSort={true}  dataFormat={this.instanceFormatter}              > Name                 </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="scientificName"       dataAlign="center"  dataSort={true}  dataFormat={this.txtFormatter}      isKey={true} > Scientific Name      </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="vulnerability"        dataAlign="center"  dataSort={true}  dataFormat={this.txtFormatter}                   > Vulnerability        </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="videoLink"            dataAlign="center"  dataSort={true}  dataFormat={this.linkFormatter}                  > Video                </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="citationLink"         dataAlign="left"    dataSort={true}  dataFormat={this.txtFormatter}                   > Citation             </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="conservationMeasure"  dataAlign="center"  dataSort={true}  dataFormat={this.txtFormatter}                   > Conservation Measure </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="webLink"              dataAlign="center"  dataSort={true}  dataFormat={this.linkFormatter}                  > Source               </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="assoc_threats"        dataAlign="left"    dataSort={true}  dataFormat={this.threatFormatter}                > Associated Threats   </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="assoc_habitats"       dataAlign="left"    dataSort={true}  dataFormat={this.habitatFormatter}               > Associated Habitats  </TableHeaderColumn>
+            <TableHeaderColumn width='200' dataField="assoc_countries"      dataAlign="left"    dataSort={true}  dataFormat={this.countryFormatter}               > Associated Countries </TableHeaderColumn>
+          </BootstrapTable>
+
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.props.onHide}>Close</Button>
@@ -39,8 +68,6 @@ const MyLargeModal = React.createClass({
     );
   },
 });
-
-
 
 export default class HomePage extends React.Component {
 
@@ -53,10 +80,24 @@ export default class HomePage extends React.Component {
 
   constructor(props) {
     super(props);
+
     this.state = {
-      lgModalShow: false
+      lgModalShow: false,
+      type: "animal",
+      model: [],
     }
-  }
+
+    var that = this;
+    axios.create({
+      baseURL: 'https://swe-endangered-animals.appspot.com/',
+      headers: {"Access-Control-Allow-Origin": "*"}
+    }).get('/all_'+this.state.type+'_data')
+      .then(function(data) {
+        that.setState({
+          model: data.data
+        });
+    });
+  };
 
   render() {
     let lgClose = () => this.setState({ lgModalShow: false });
@@ -71,7 +112,7 @@ export default class HomePage extends React.Component {
           <Button type="submit" onClick={ ()=> {this.handleClick()}} >
             Search
           </Button>
-          <MyLargeModal show={this.state.lgModalShow} onHide={lgClose} />
+          <MyLargeModal show={this.state.lgModalShow} onHide={lgClose} model={this.state.model} />
 
         <div>
           <Carousel>
